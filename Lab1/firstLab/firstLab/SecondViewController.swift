16c4450711cb7523550606417a6fc899298e0010
Ross Blassingame <ross.blassingame@colorado.edu>
2018-02-07 13:13:40 -0700

    Done with second tab - open app.

--- Lab1/firstLab/firstLab/SecondViewController.swift
+++ Lab1/firstLab/firstLab/SecondViewController.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+	
+	@IBAction func openSafariButton(_ sender: Any) {
+		//check to see if there's an app installed to handle this URL scheme
+		if(UIApplication.shared.canOpenURL(URL(string: "safari://")!)){
+			//open the app with this URL scheme
+			UIApplication.shared.open(URL(string: "safari://")!, options: [:], completionHandler: nil)
+		}else {
+			UIApplication.shared.open(URL(string: "http://www.google.com/")!, options: [:], completionHandler: nil)
+		}
+	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
