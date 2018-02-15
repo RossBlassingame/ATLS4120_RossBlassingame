7360c7b8f0e1538caeb89c515b3ba96144743868
Ross Blassingame <ross.blassingame@colorado.edu>
2018-02-15 12:09:58 -0700

    Added milestone 1.

---
+++ Project/BudgetR/BudgetR/AccountViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AccountViewController.swift
+//  BudgetR
+//
+//  Created by Ross Blassingame on 2/14/18.
+//  Copyright © 2018 Ross Blassingame. All rights reserved.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+	
+	//MARK: Properties
+	@IBOutlet weak var accountNameLabel: UILabel!
+	@IBOutlet weak var accountBalanceLabel: UILabel!
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	
+}
