c63f1104dd66c2438b01c16be77a358253b6f9c2
Ross Blassingame <ross.blassingame@colorado.edu>
2018-02-28 13:02:00 -0700

    updated project

--- Project/BudgetR/BudgetR/BudgetViewController.swift
+++ Project/BudgetR/BudgetR/BudgetViewController.swift
@@ -9,17 +9,25 @@
 import UIKit
 
 class BudgetViewController: UIViewController {
+	
+	@IBOutlet weak var toBeBudgetedLabel: UILabel!
+	@IBOutlet weak var budgetAmountLabel: UILabel!
+	
+	var budgetAmount: Double = 0
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		toBeBudgetedLabel.layer.borderWidth = 1
+		toBeBudgetedLabel.layer.cornerRadius = 4
+		budgetAmountLabel.layer.borderWidth = 1
+		budgetAmountLabel.layer.cornerRadius = 4
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
 
 }
 
