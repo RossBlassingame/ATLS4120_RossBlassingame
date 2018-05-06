31457a872d8dee4279a982e61f7b6184ac14d4e5
Ross Blassingame <ross.blassingame@colorado.edu>
2018-05-06 16:40:39 -0600

    60 pts

---
+++ final/v2/app/src/main/java/com/example/rb/animals/AnimalCategoryActivity.java
@@ -0,0 +1,77 @@
+package com.example.rb.animals;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by rb on 4/6/18.
+ */
+
+public class AnimalCategoryActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = getIntent();
+        String animaltype = i.getStringExtra("animaltype");
+
+        //get the list view
+        ListView listAnimals = getListView();
+
+        //define an array adapter
+        ArrayAdapter<Animal> listAdapter;
+
+        //initialize the array adapter with the right list of animals
+        switch (animaltype){
+            case "Dogs":
+                listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Animal.dogs);
+                break;
+            case "Strength":
+                listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Animal.strength);
+                break;
+            case "Flexibility":
+                listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Animal.flexibility);
+                break;
+            default: listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Animal.dogs);
+        }
+
+        //set the array adapter on the list view
+        listAnimals.setAdapter(listAdapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView listView, View view, int position, long id){
+        Intent intent = new Intent(AnimalCategoryActivity.this, AnimalActivity.class);
+        intent.putExtra("animalid", (int) id);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflate menu to add items to the action bar
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //get the ID of the item on the action bar that was clicked
+        switch (item.getItemId()) {
+            case R.id.create_order:
+                //start order activity
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
---
+++ final/v2/app/src/main/java/com/example/rb/animals/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.rb.animals;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //create listener
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> listView, View view, int position, long id){
+                String animaltype = (String) listView.getItemAtPosition(position);
+                //create new intent
+                Intent intent = new Intent(MainActivity.this, AnimalCategoryActivity.class);
+                //add animaltype to intent
+                intent.putExtra("animaltype", animaltype);
+                //start intent
+                startActivity(intent);
+            }
+        };
+
+        //get the list view
+        ListView listview = findViewById(R.id.listView);
+        //add listener to the list view
+        listview.setOnItemClickListener(itemClickListener);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflate menu to add items to the action bar
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //get the ID of the item on the action bar that was clicked
+        switch (item.getItemId()) {
+            case R.id.create_order:
+                //start order activity
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
